6cf495b8c87d5277c966d135d07e858c704abd92
Kemal YILDIRIM <kemalyldrim@gmail.com>
2019-11-04 01:40:11 +0000

    Add project files.

---
+++ Blog/Controllers/PostController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Blog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        private readonly ILogger<PostController> _logger;
+        public PostController(ILogger<PostController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Create()
+        {
+
+        }
+    }
+}
