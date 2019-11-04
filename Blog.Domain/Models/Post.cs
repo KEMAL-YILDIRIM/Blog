95b451af1bf176fd9f86a99f1b99ac1f42c38770
Kemal YILDIRIM <kemalyldrim@gmail.com>
2019-11-04 14:47:04 +0000

    blog structure initialised

---
+++ Blog.Domain/Models/Post.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Blog.Domain.Models
+{
+    public class Post
+    {
+        public Post()
+        {
+
+        }
+
+        public DateTime CreateDate { get; set; }
+        public TimeSpan ReadingTime { get; set; } = new TimeSpan();
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public string Body { get; set; }
+
+    }
+}
---
+++ Blog.Domain/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog.Domain.Models
+{
+    public class User
+    {
+        public User()
+        {
+
+        }
+
+        public string FullName { get; set; }
+        public DateTime RegisteredAt { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Password { get; set; }
+    }
+}
