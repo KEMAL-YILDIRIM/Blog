fe3c1b6ae8039a70d02b5689e5b82a3496d2e879
Kemal Yildirim <kemal.yildirim@racelogic.co.uk>
2020-04-26 23:05:03 +0100

    repositories removed validation added to create user

---
+++ Blog.Logic/UserAggregate/Commands/CreateUser/CreateUserRequestValidateCollection.cs
@@ -0,0 +1,18 @@
+﻿using Blog.Logic.Validators;
+
+using FluentValidation;
+
+namespace Blog.Logic.UserAggregate.Commands.CreateUser
+{
+	public class CreateUserRequestValidateCollection : AbstractValidator<CreateUserRequest>
+	{
+		public CreateUserRequestValidateCollection(
+			IPasswordValidator passwordValidator,
+			IEmailValidator emailValidator)
+		{
+			RuleFor(x => x.FullName).Length(5).NotEmpty();
+			RuleFor(x => x.Email).EmailAddress().Must(emailValidator.IsValid).NotEmpty();
+			RuleFor(x => x.Password).Must(passwordValidator.Validate).NotEmpty();
+		}
+	}
+}
